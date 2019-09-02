e2f5e1943b7c312d5ed729d327253436d0e0383c
Fábio Luiz Esperati Pagoti <fabiopagoti@gmail.com>
2019-09-02 21:09:20 +0000

    countries list and busy indicator in the page

--- webapp/controller/View1.controller.js
+++ webapp/controller/View1.controller.js
@@ -1,38 +1,62 @@
 sap.ui.define([
 	"sap/ui/core/mvc/Controller",
-	"sap/suite/ui/commons/TimelineItem"
-], function (Controller, TimelineItem) {
+	"sap/suite/ui/commons/TimelineItem",
+	"sap/ui/core/Item"
+], function (Controller, TimelineItem, Item) {
 	"use strict";
 
 	return Controller.extend("ovly.holidays.controller.View1", {
-		
+
 		endPoint: "https://calendarific.com/api/v2/holidays",
 		apiKey: "453f7bd6deb5ac7481a2b52ba8061a6592ed29c3",
-		
+
 		onInit: function () {
+			this._page = this.byId("page");
 			this._select = this.byId("select");
 			this._datePicker = this.byId("datepicker");
 			this._timeline = this.byId("timeline");
+			
+			this._fillSelect();
+
 		},
-		
-		onSearch: function(oEvent){
-			$.get(this.endPoint, this._getParameters(), function(oResponse){
+
+		onSearch: function (oEvent) {
+			this._page.setBusy(true);
+			$.get(this.endPoint, this._getParameters(), function (oResponse) {
 				this._refreshTimeline(oResponse.response.holidays);
+				this._page.setBusy(false);
 			}.bind(this));
 		},
-		
-		_getParameters: function(){
+
+		_fillSelect: function () {
+			var sPath = sap.ui.require.toUrl("ovly/holidays/model/countries.json");
+			$.get(sPath, function (aCountries) {
+
+				$.each(aCountries, function (index, oCountry) {
+					var oNewItem = new Item({
+						key: oCountry.id,
+						text: oCountry.name
+					});
+					
+					this._select.addItem(oNewItem);
+
+				}.bind(this));
+				
+			}.bind(this));
+		},
+
+		_getParameters: function () {
 			return {
 				api_key: this.apiKey,
 				country: this._select.getSelectedKey(),
 				year: this._datePicker.getDateValue().getFullYear()
 			}
 		},
-		
-		_refreshTimeline: function(aHolidays){
+
+		_refreshTimeline: function (aHolidays) {
 			this._timeline.removeAllContent();
-			
-			jQuery.each(aHolidays, function(index, element){
+
+			jQuery.each(aHolidays, function (index, element) {
 				var oTimelineItem = new TimelineItem({
 					dateTime: element.date.iso,
 					title: element.name,
